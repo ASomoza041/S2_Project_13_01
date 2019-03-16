2f7f00d6c652c6a6f686c9dbf868720cf0024005
Alex Somoza <ASomoza041@west-mec.org>
2019-03-15 18:13:57 -0700

    Final commit
    Created functions to change the image of the stars when the mouse is on the image, added a function to check the number of characters, added an if else statement to change the colors of the character counter when we pass 1000 characters Ready for review.

--- bw_review.js
+++ bw_review.js
@@ -6,7 +6,7 @@
    Case Problem 1
 
    Author: Alex A. Somoza 
-   Date:   
+   Date: 3-15-19  
    
    Filename: bw_review.js
 	
@@ -35,11 +35,11 @@
 */
 
 //run the init function when page is loaded
-window.onload = init();
+window.onload = init;
 
 //Create a function to define event listeners used in the page.
 function init() {
-      var stars = document.querySelectorAll("span#stars");
+      var stars = document.querySelectorAll("span#stars img");
 
       for (var i = 0; i < stars.length; i++) {
             stars[i].style.cursor = "pointer";
@@ -47,6 +47,53 @@ function init() {
       }
       document.getElementById("comment").addEventListener("keyup", updateCount);
 }
+
+//Make stars light up when user hovers mouse over the empty stars.
+function lightStars(e) {
+      var starNumber = e.target.alt;
+      var stars = document.querySelectorAll("span#stars img");
+
+      //lights up stars when hovered over.
+      for (var i = 0; i < starNumber; i++) {
+            stars[i].src = "bw_star2.png";
+      }
+
+      for (i = starNumber; i < 5; i++) {
+            stars[i].src = "bw_star.png";
+      }
+      document.getElementById("rating").value = starNumber + " star(s)";
+      e.target.addEventListener("mouseleave", function () {
+            e.target.removeEventListener("mouseleave", turnOffStars);
+      });
+}
+
+//unlight stars when the mouse pointer is off the image.
+function turnOffStars() {
+      var stars = document.querySelectorAll("span#stars img");
+
+      for (var i = 0; i < starNumber; i++) {
+            stars[i].src = "bw_star.png";
+      }
+      document.getElementById("rating").value = "";
+}
+
+//tracks number of characters typed into the text area box.
+function updateCount() {
+      var commentText = document.getElementById("comment").value;
+      var charCount = countCharacters(commentText);
+      var wordCountBox = document.getElementById("wordCount");
+      wordCountBox.value = charCount + "/1000";
+
+      //Changes the color of the text and background when it reaches over 1000 characters.
+      if (charCount > 1000) {
+            wordCountBox.style.color = "white";
+            wordCountBox.style.backgroundColor = "red";
+      } else {
+            wordCountBox.style.color = "black";
+            wordCountBox.style.backgroundColor = "white";
+      }
+}
+
 
 
 
